e408438da79729350c7b177b4cfa744c5d599e33
Jamil Irshad <jamil.irshad32@gmail.com>
2024-03-10 16:09:15 +0500

    Added email to help desk using contact form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 //For current year
 var year = document.getElementById("year");
 year.innerHTML = new Date().getFullYear();
+
+//Contact US form handling
+
+
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
